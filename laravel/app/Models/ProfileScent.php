705f06e619cf938523eb1adcee23d195ef3efa34
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-08 23:17:24 +0000

    feat: added empty structures of all models

---
+++ laravel/app/Models/ProfileScent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProfileScent extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileScentFactory> */
+    use HasFactory;
+}
---
+++ laravel/app/Models/UserFragranceStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserFragranceStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFragranceStatusFactory> */
+    use HasFactory;
+}
