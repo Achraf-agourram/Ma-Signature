0595b74b82b70d470077957772254ad12d4c076e
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-08 23:19:42 +0000

    feat: added empty structures of all seeders

---
+++ laravel/database/seeders/FragranceFamilySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FragranceFamilySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
