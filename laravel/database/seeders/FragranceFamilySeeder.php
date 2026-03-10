1bb38d6dc61c9f38215ffb747cf58a9df2db384d
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-10 21:29:32 +0000

    feat: added FragranceFamilySeeder

--- laravel/database/seeders/FragranceFamilySeeder.php
+++ laravel/database/seeders/FragranceFamilySeeder.php
@@ -2,16 +2,67 @@
 
 namespace Database\Seeders;
 
+use App\Models\FragranceFamily;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class FragranceFamilySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
-        //
+        $families = [
+            'Citrus',
+            'Floral',
+            'White Floral',
+            'Yellow Floral',
+            'Rose',
+            'Woody',
+            'Sandalwood',
+            'Cedar',
+            'Amber',
+            'Oriental',
+            'Gourmand',
+            'Sweet',
+            'Vanilla',
+            'Fruity',
+            'Green',
+            'Aquatic',
+            'Aromatic',
+            'Powdery',
+            'Musky',
+            'Leather',
+            'Oud',
+            'Spicy',
+            'Warm Spicy',
+            'Fresh Spicy',
+            'Balsamic',
+            'Aldehydic',
+            'Honey',
+            'Animalic',
+            'Smoky',
+            'Earthy',
+            'Herbal',
+            'Metallic',
+            'Salty',
+            'Mineral',
+            'Tea',
+            'Coffee',
+            'Chocolate',
+            'Caramel',
+            'Lactonic',
+            'Nutty',
+            'Tobacco',
+            'Rum',
+            'Whiskey',
+            'Champagne',
+            'Soft Spicy'
+        ];
+
+        foreach ($families as $family) {
+            FragranceFamily::create([
+                'name' => $family
+            ]);
+        }
     }
 }
